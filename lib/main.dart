f5879182eef5f1cdd3a72e1043bed15a2bb91420
ds4914 <108384004+ds4914@users.noreply.github.com>
2024-07-29 11:57:45 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:pixelapp/constants/route_list.dart';
 import 'package:pixelapp/presentation/ui/customer_list_page/customer_list_page.dart';
 import 'package:pixelapp/presentation/ui/registration_page/registration_page.dart';
 
-void main() {
+void main()async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
